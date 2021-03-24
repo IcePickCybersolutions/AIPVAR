e16258a892d05719b7dccb783656a9d3de039112
IcePickCyberSolutions <80115033+IcePickCybersolutions@users.noreply.github.com>
2021-03-24 17:15:37 -0600

    Update rasputin.py

--- rasputin.py
+++ rasputin.py
@@ -1,11 +1,13 @@
-'''I'd like to thank you for reading my code, pls be nice tho ;)
+'''
+I'd like to thank you for reading my code, pls be nice tho ;)
 If you're a little newer to this whole coding thingy,
 don't stress, theres a metric ton of comments that I think explain it all rather nicely.
 And if you are more experienced, feel free to shoot me a message or comment on anything that I could've done better
 '''
 
-'''Just importing a bunch of dependencies so the damn thing works
-Check out the venv folder on my github page for how to set up a virtual environment for this script to work.
+''' 
+Just importing a bunch of dependencies so the damn thing works.
+Check out the venv folder on my github page for how to set up a virtual environment for this script to work. 
 '''
 import speech_recognition as sr
 import pyttsx3
@@ -19,17 +21,17 @@ import wolframalpha
 import json
 import requests
 
-#starting up the speech recognition engine (sapi5) which only works on windows
+# starting up the speech recognition engine (sapi5) which only works on windows
 engine=pyttsx3.init('sapi5')
 voices=engine.getProperty('voices')
 engine.setProperty('voice','voices[0].id')
 
-#function for how the engine should speak
+# function for how the engine should speak
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-#function for basic pleasantries
+# function for basic pleasantries
 def wishMe():
     hour=datetime.datetime.now().hour
     if hour>=0 and hour<12:
@@ -42,7 +44,7 @@ def wishMe():
         speak("Hello, Good Evening")
         print("Hello, Good Evening")
 
-#this takes input from speech and responds to it with actions defined later
+# this takes input from speech and responds to it with actions defined later
 def takeCommand():
     r=sr.Recognizer()
     with sr.Microphone() as source:
@@ -53,19 +55,19 @@ def takeCommand():
             statement=r.recognize_google(audio,language='en-in')
             print(f"user said:{statement}\n")
 
-		#if it can't understand you it asks you to say it again
+	# if it can't understand you it asks you to say it again
         except Exception as e:
             speak("Repeat instructions")
         return "None"
     return statement
-#and thats the end of our functions
+# and thats the end of our functions
 
-#when the machine starts up it should introduce itself and greet you
+# when the machine starts up it should introduce itself and greet you
 print("Loading in Artificially Intelligent Personal Assistant Rasputin")
 speak("Loading in AIPAR")
 wishMe()
 
-#makes sure that it's still active, and if so offers its services
+# makes sure that it's still active, and if so offers its services
 if __name__=='__main__':
     while True:
         speak("Issue further commands.")
@@ -73,18 +75,20 @@ if __name__=='__main__':
         if statement==0:
             continue
 
-		#will shut down the program if you ask it to
+	# will shut down the program if you ask it to
         if "good bye" or "see you later" or "have a good one" or "stop" in statement:
             speak("Good Night")
             print("Exiting AIPA Rasputin")
             break
 
-#from here we define commands and what they will do
-'''To write your own command use this 'formula'
+# from here we define commands and what they will do
+'''
+
+To write your own command use this 'formula':
 
 if "spoken-command" or "alternative spoken command" in statement:
 	speak("What you want Rasputin to say")
-	commandstotellrasputinwhatyouwantdonewhenusaythat
+	commands-to-tell-rasputin-what-you-want-done-when-u-say-that
 	time.slee(5)
 
 
@@ -94,7 +98,7 @@ Here are some general commands to do stuff:
  -
 
 '''
-        #will search wiki and both speak and pring out the results
+        # will search wiki and will both speak and print out the results
         if 'wikipedia' or 'wiki' in statement:
             speak("Searching Wikipedia...")
             statement =statement.replace("wikipedia", "")
@@ -103,12 +107,12 @@ Here are some general commands to do stuff:
             print(results)
             speak(results)
 
-        elif 'open youtube' in statement:
+        elif 'open youtube' or 'tube time' in statement:
             webbrowser.open_new_tab("https://www.youtube.com")
             speak("youtube is now open")
             time.sleep(5)
 
-        #this doesn't actually search anything, but simply opens the google webpage
+        # this doesn't actually search anything, but simply opens the google webpage
         elif 'open google' or 'browse' or 'google' in statement:
             webbrowser.open_new_tab("https://google.com")
             speak("Search is ready")
@@ -119,30 +123,19 @@ Here are some general commands to do stuff:
             speak("Emails prepared")
             time.sleep(5)
 
-        #i do love my sc, so this is just a fun way to open a website
-        elif 'robert space industries' or 'visit Mr.Roberts' or 'RSI' or 'sc' in statement:
-            webbrowser.open_new_tab("https://robertsspaceindustries.com/")
-            speak("AEGIS COMBAT ASSIST ENABLED")
-            time.sleep(5)
-
-        #more sc stuff
-        elif 'gallog' or 'galactic logistics' or 'space trucking' or 'trading' in statement:
-            webbrowser.open_new_tab("https://www.gallog.co/")
-            speak("Trading data at hand")
-            time.sleep(5)
-
         elif 'drive' or 'docs' or 'my drive' in statement:
             webbrowser.open_new_tab("https://drive.google.com/drive/u/0/my-drive")
             speak("Google Docs is loaded")
             time.sleep(5)
 
+	# if you're gonna comment on this i already know u have an android
         elif 'icloud' in statement:
             webbrowser.open_new_tab("https://www.icloud.com/")
             speak("Apple cloud services operational")
             time.sleep(5)
 
-        # i love this site, use it for all sorts of stuff
-        # it's basically a schedule app
+    ''' i love this site, use it for all sorts of stuff
+        it's basically a schedule app '''
         elif 'pipefy' or 'kapkan' or 'kanban' or 'pipes' in statement:
             webbrowser.open_new_tab("https://app-auth.pipefy.com/login")
             speak("Pipes are read to be clogged")
@@ -203,7 +196,7 @@ Here are some general commands to do stuff:
             speak("I've got my eye on you...")
             time.sleep(5)
 
-		#just tells you what the time is
+	# just tells you what the time is
         elif "time" in statement:
             strTime=datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"the time is {strtime}")
@@ -214,24 +207,22 @@ Here are some general commands to do stuff:
         elif "who made you" or "who created you" or "who's your daddy" or "who discovered you" or "who's your maker" in statement:
             speak("Code is rarely owned, friend.")
 
-		'''will search the news sites: national post, new york times, and the wall street journal.
-		Of course, feel free to add more sites to open, or replace mine
-		'''
+    ''' will search the news sites: national post, new york times, and the wall street journal.
+    	Of course, feel free to add more sites to open, or replace mine '''
         elif "news" or "whats going on in the world" or "whats new" in statement:
             news = webbrowser.open_new_tab("https://nationalpost.com"), webbrowser.open_new_tab("https://www.nytimes.com/"), webbrowser.open_new_tab("https://www.wsj.com/")
             speak("Here are some headlines from Naitonal Post, the Times, and WSJ")
             time.sleep(6)
 
-		'''just puts in a search on the web if you ask it to
-		for example say "search for orange-flavoured chocolate" and rasputin will search for that
-		'''
+    '''	just puts in a search on the web if you ask it to
+	for example say "search for orange-flavoured chocolate" and rasputin will search for that '''
         elif "search" in statement:
             statement = statement.replace("Search", "")
             webbrowser.open_new_tab(statement)
             time.sleep(5)
 
-		#this uses the WolframAlpha API to answer questions that you have, check out the WolfRamAlpha file in my repo to learn about this
-		#To use this best say something along the lines of "I want to ask something", and upon the prompt, ask away
+     ''' this uses the WolframAlpha API to answer questions that you have, check out the WolfRamAlpha file in my repo to learn about this
+	 To use this best say something along the lines of "I want to ask something", and upon the prompt, ask away '''
         elif "ask" in statement:
             speak('What question do you have for me?')
             question = takeCommand()
@@ -242,7 +233,7 @@ Here are some general commands to do stuff:
             speak(answer)
             print(answer)
 
-		#the command I always want siri to have, to power down
+	# what i try and ask siri to do at least once a day, don't worry i'll keep trying
         elif "log off" or "sign out" or "power off" or "shut down" in statement:
             speak("Ok, your pc will log of in 10 seconds, exit all applications.")
             subprocess.call(["shutdown", "/l"])
